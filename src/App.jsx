238fc4ccfc6189a890aeb8081ad00c1d3bd4ee4b
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-05 01:37:57 +0530

    Add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginPage from './pages/Login';
 import RegistrationPage from './pages/Register';
